767af151b3e7a4695b3db2d039201b9049190bd4
Piyush Prakash <piyushprakash2104@gmail.com>
2026-04-17 15:03:57 +0530

    connect frontend to deployed backend

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -198,7 +198,7 @@ export default function Home() {
       setStatus("Submitting bulk logs...");
       const parsedLogs = JSON.parse(bulkLogs);
 
-      const res = await fetch("http://127.0.0.1:8000/logs/bulk", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/logs/bulk`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -253,7 +253,7 @@ export default function Home() {
         };
       });
 
-      const res = await fetch("http://127.0.0.1:8000/logs/bulk", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/logs/bulk`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
